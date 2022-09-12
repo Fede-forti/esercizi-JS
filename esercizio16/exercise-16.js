00a3b9394f082b16f41b3403741497c259b313a5
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-09-12 15:17:10 +0200

    exercise done

---
+++ esercizio16/exercise-16.js
@@ -0,0 +1,22 @@
+function createStore() {
+  let Store = [];
+  return function (dress) {
+    Store.push(dress);
+    console.log(Store);
+    return Store;
+  }
+
+}
+
+const redPants = { id: 1, name: 'Red Pants' };
+const whiteHat = { id: 2, name: 'White Hat' };
+const blackSneakers = { id: 3, name: 'Black Sneakers' };
+
+console.log('--- Dress Store ---');
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat)
+
+console.log('--- Shoes Store ---');
+const shoesStore = createStore();
+shoesStore(blackSneakers);
