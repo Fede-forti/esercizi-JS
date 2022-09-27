7160e7d5a90df2afa12003915c4213ee710ac04d
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-09-27 16:19:08 +0200

    exercise done

---
+++ esercizio18/exercise-18.js
@@ -0,0 +1,24 @@
+function memoize(fn) {
+  let cache = {};
+  return function (num) {
+    if (num in cache) {
+      return num
+    }
+    
+  const newFactorial = fn(num);
+    return newFactorial;
+  }
+}
+
+function factorial(x) {
+  if (x === 0) {
+    return 1;
+  }
+
+  return x * factorial(x - 1);
+}
+
+let factory = memoize(factorial);
+console.log(factory(10));
+console.log(factory(6));
+console.log(factory(5));
